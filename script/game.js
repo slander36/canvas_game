a291f449350a6bb4bae95422d8f6383d97dcd3ee
Sean Lander <slander36@gmail.com>
2011-09-04 16:31:23 -0500

    adding lots of stuff new buttons/labels control pad made it resolution-independent (mostly)

--- script/game.js
+++ script/game.js
@@ -25,12 +25,42 @@ fsg.game = function() {
 		this.paused = new fsg.actor();
 		this.paused.init(this.scene);
 		this.paused.loadImage('images/paused.png');
-		this.paused.x = 320-48;
-		this.paused.y = 240-16;
+		this.paused.x = (this.canvas.width/2)-48;
+		this.paused.y = (this.canvas.height/2)-16;
+		
+		this.lifeLabel = new fsg.actor();
+		this.lifeLabel.init(this.scene);
+		this.lifeLabel.loadImage('images/lifelabel.png');
+		this.lifeLabel.x = 0;
+		this.lifeLabel.y = 0;
+		
+		this.levelLabel = new fsg.actor();
+		this.levelLabel.init(this.scene);
+		this.levelLabel.loadImage('images/levellabel.png');
+		this.levelLabel.x = (this.canvas.width/2)-64;
+		this.levelLabel.y = 0;
+		
+		this.skipSceneButton = new fsg.actor();
+		this.skipSceneButton.init(this.scene);
+		this.skipSceneButton.loadImage('images/skiplevelbutton.png');
+		this.skipSceneButton.x = this.canvas.width-128;
+		this.skipSceneButton.y = 0;
+		
+		this.pauseButton = new fsg.actor();
+		this.pauseButton.init(this.scene);
+		this.pauseButton.loadImage('images/pausebutton.png');
+		this.pauseButton.x = this.canvas.width-128;
+		this.pauseButton.y = this.canvas.height-32;
+		
+		this.gamepad = new fsg.actor();
+		this.gamepad.init(this.scene);
+		this.gamepad.loadImage('images/gamepad.png');
+		this.gamepad.x = 0;
+		this.gamepad.y = this.canvas.height-96;
 		
 	};
 	
-	this.update = function(paused, modifier, keysDown, skipScene) {
+	this.update = function(paused, modifier, keysDown, gamepad, skipScene) {
 		
 		this.isPaused = paused;
 		
@@ -46,14 +76,26 @@ fsg.game = function() {
 			return;
 		}
 		
-		if (38 in keysDown)
+		var keyDown = keysDown[keysDown.length-1];
+		
+		this.gamepad.loadImage('images/gamepad.png');
+		
+		if (38 == keyDown || gamepad == "up") {
 			this.player.moveUp();
-		if (40 in keysDown)
+			this.gamepad.loadImage('images/gamepadup.png');
+		}
+		if (40 == keyDown || gamepad == "down") {
 			this.player.moveDown();
-		if(37 in keysDown)
+			this.gamepad.loadImage('images/gamepaddown.png');
+		}
+		if(37 == keyDown || gamepad == "left") {
 			this.player.moveLeft();
-		if(39 in keysDown)
+			this.gamepad.loadImage('images/gamepadleft.png');
+		}
+		if(39 == keyDown || gamepad == "right") {
 			this.player.moveRight();
+			this.gamepad.loadImage('images/gamepadright.png');
+		}
 		
 		this.player.move(modifier);
 		
@@ -108,47 +150,55 @@ fsg.game = function() {
 
 	this.render = function() {
 		
-		for(i in this.scene.drawmap) {
+		for (i in this.scene.drawmap) {
 			this.ctx.drawImage(this.scene.tilemap[this.scene.drawmap[i]],i%20*32,Math.floor(i/20)*32);
 		}
 		
 		if (this.player.actorImage.actorReady) {
 			this.ctx.drawImage(this.player.actorImage, this.player.x, this.player.y);
-		};
+		}
 		
-		for(i in this.monsters) {
+		for (i in this.monsters) {
 			this.ctx.drawImage(this.monsters[i].actorImage,this.monsters[i].x,this.monsters[i].y);
 		}
 		
+		if (this.lifeLabel.actorImage.actorReady) {
+			this.ctx.drawImage(this.lifeLabel.actorImage, this.lifeLabel.x, this.lifeLabel.y);
+		}
+		
+		if (this.levelLabel.actorImage.actorReady) {
+			this.ctx.drawImage(this.levelLabel.actorImage, this.levelLabel.x, this.levelLabel.y);
+		}
+		
+		if (this.skipSceneButton.actorImage.actorReady) {
+			this.ctx.drawImage(this.skipSceneButton.actorImage, this.skipSceneButton.x, this.skipSceneButton.y);
+		}
+		
+		if (this.pauseButton.actorImage.actorReady) {
+			this.ctx.drawImage(this.pauseButton.actorImage, this.pauseButton.x, this.pauseButton.y);
+		}
+		
+		if (this.gamepad.actorImage.actorReady) {
+			this.ctx.drawImage(this.gamepad.actorImage, this.gamepad.x, this.gamepad.y);
+		}
+		
 		this.ctx.fillStyle = "rgb(250,250,250)";
-		this.ctx.font = "24px Helvetica";
-		this.ctx.textAlign = "left";
+		this.ctx.font = "bold 20px Courier New";
+		this.ctx.textAlign = "center";
 		this.ctx.textBaseline = "top";
-		this.ctx.fillText("Level: " + (this.scene.id+1),0,480-32);
+		this.ctx.fillText((this.scene.id+1),(this.canvas.width/2)+32,6);
 		
-		this.ctx.fillStyle = "rgb(200,200,200)";
-		this.ctx.font = "18px Helvetica";
-		this.ctx.textAlign = "left";
-		this.ctx.textBaseline = "top";
-		this.ctx.fillText("0: " + this.player.moveQueue[0], 640-128, 32);
+//		this.ctx.fillStyle = "rgb(250,250,250)";
+//		this.ctx.font = "18px Helvetica";
+//		this.ctx.textAlign = "left";
+//		this.ctx.textBaseline = "top";
+//		this.ctx.fillText("0: " + this.player.moveQueue[0], 640-128-96, 0);
 		
-		this.ctx.fillStyle = "rgb(200,200,200)";
-		this.ctx.font = "18px Helvetica";
+		this.ctx.fillStyle = "rgb(250,250,250)";
+		this.ctx.font = "bold 20px Courier New";
 		this.ctx.textAlign = "left";
 		this.ctx.textBaseline = "top";
-		this.ctx.fillText("Life: " + this.player.health, 32, 64);
-
-		this.ctx.fillStyle = "rgb(200,200,200)";
-		this.ctx.font = "18px Helvetica";
-		this.ctx.textAlign = "left";
-		this.ctx.textBaseline = "top";
-		this.ctx.fillText("Skip Scene", 640-96, 0);
-
-		this.ctx.fillStyle = "rgb(200,200,200)";
-		this.ctx.font = "18px Helvetica";
-		this.ctx.textAlign = "left";
-		this.ctx.textBaseline = "top";
-		this.ctx.fillText("Pause", 640-64, 480-32);
+		this.ctx.fillText(this.player.health, 64+16, 6);
 		
 		if(this.isPaused) {
 			this.ctx.drawImage(this.paused.actorImage, this.paused.x, this.paused.y);
--- script/main.js
+++ script/main.js
@@ -9,32 +9,65 @@ fsg.main = function() {
 	canvas.height = 480;
 	document.getElementById('game').appendChild(canvas);
 	
-	var keysDown = {};
+	var keysDown = [];
 	
 	var paused = false;
 	
+	var gamepad = null;
+	
 	addEventListener("keydown", function(e) {
-		keysDown[e.keyCode] = true;
+		if(keysDown.indexOf(e.keyCode) < 0)
+			keysDown.push(e.keyCode);
 	}, false);
 	
 	addEventListener("keyup", function(e) {
-		delete keysDown[e.keyCode];
+		if(keysDown.indexOf(e.keyCode) >= 0)
+			keysDown.splice(keysDown.indexOf(e.keyCode),1);
 	}, false);
 	
 	var skipScene = false;
 	
+	var ox = 48;
+	var oy = canvas.height - 48;
+	
 	addEventListener("mousedown", function(e) {
-		if((e.pageX > (640-96) && e.pageX < 640)
-				&& (e.pageY < 64 && e.pageY > 0)) {
+		var x = e.pageX;
+		var y = e.pageY;
+		
+		// Check if SkipLevel Pressed
+		if((x > (canvas.width-128) && x < canvas.width)
+				&& (y < 32 && y > 0)) {
 			skipScene = true;
 		}
-		if((e.pageX > (640-64) && e.pageX < 640 )
-				&& (e.pageY > (480-32) && e.pageY < 480)) {
+		
+		// Check if Pause Pressed
+		if((x > (canvas.width-128) && x < canvas.width)
+				&& (y > (canvas.height-32) && y < canvas.height)) {
 			if(paused == true)
 				paused = false;
 			else
 				paused = true;
 		}
+		
+		// Create x & y relative to gamepad
+		var rx = x - ox;
+		var ry = y - oy;
+		
+		// Check if gamepad being used
+		if ((0 < x && x < (ox + 48)) && ((oy - 48) < y && y < canvas.height)) {
+			if(ry < 0 && Math.abs(ry) > Math.abs(rx))
+				gamepad = "up";
+			if(ry > 0 && Math.abs(ry) > Math.abs(rx))
+				gamepad = "down";
+			if(rx > 0 && Math.abs(rx) > Math.abs(ry))
+				gamepad = "right";
+			if(rx < 0 && Math.abs(rx) > Math.abs(ry))
+				gamepad = "left";
+		}
+	});
+	
+	addEventListener("mouseup", function(e) {
+		gamepad = null;
 	});
 	
 	var game = new fsg.game();
@@ -46,16 +79,16 @@ fsg.main = function() {
 	setInterval(function() {
 		var now = Date.now();
 		var delta = now - then;
-		game.update(paused, delta/1000, keysDown, skipScene);
+		game.update(paused, delta/1000, keysDown, gamepad, skipScene);
 		skipScene = false;
 		game.render();
 		
 		// FPS
-		ctx.fillStyle = "rgb(200,200,200)";
+		ctx.fillStyle = "rgb(250,250,250)";
 		ctx.font = "18px Helvetica";
 		ctx.textAlign = "left";
 		ctx.textBaseline = "top";
-		ctx.fillText("FPS: "+(1000/delta).toFixed(2), 32, 32);
+		ctx.fillText("FPS: "+(1000/delta).toFixed(2), 96, 0);
 		
 		then = now;
 	}, 1);
