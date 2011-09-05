be7dcaab1f0322cdeeaad6b994f830f3a0a90a20
Sean Lander <slander36@gmail.com>
2011-09-04 22:27:38 -0500

    adding monsters back in minor update to troll model

--- script/monsterlist.js
+++ script/monsterlist.js
@@ -2,10 +2,10 @@ fsg.monsterlist = [];
 
 fsg.monsterlist[0] =
 	[
-//	{	start:	[6,6],
-//		image: 	"images/goblin.png",
-//		name:	"Goblin"
-//	},
+	{	start:	[6,6],
+		image: 	"images/goblin.png",
+		name:	"Goblin"
+	},
 //	{	start:	[7,11],
 //		image:	"images/goblin.png",
 //		name:	"Goblin"
@@ -14,11 +14,11 @@ fsg.monsterlist[0] =
 
 fsg.monsterlist[1] =
 	[
-//	{
-//		start:	[3,5],
-//		image:	"images/troll.png",
-//		name:	"Troll"
-//	},
+	{
+		start:	[3,5],
+		image:	"images/troll.png",
+		name:	"Troll"
+	},
 //	{
 //		start:	[8,11],
 //		image:	"images/troll.png",
